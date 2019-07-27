1f4a30a150a8dd62a938ea2cdf30e4104fc18845
Christian Lamothe <c.lamothe25@gmail.com>
2019-07-27 13:47:22 -0400

    Created car class and defined routes

--- 3-api/apistar/app.py
+++ 3-api/apistar/app.py
@@ -17,13 +17,17 @@ VALID_MANUFACTURERS = set([car["manufacturer"]
                           
 CAR_NOT_FOUND = 'Car not found'
 
-breakpoint()
+# breakpoint()
 # load in data
 
 # definition
 
 class Car(types.Type):
-    pass
+    id = validators.Integer(allow_null=True) # assign in POST
+    manufacturer = validators.String(enum=list(VALID_MANUFACTURERS))
+    model = validators.String(max_length=50)
+    year = validators.Integer(minimum=1900, maximum=2050)
+    vin = validators.String(max_length=50, default='')
 
 # API methods
 
@@ -35,4 +39,23 @@ def create_car(car: Car) -> JSONResponse:
 
 def get_car(car_id: int) -> JSONResponse:
     pass
+
+def update_car(car_id: int, car: Car) -> JSONResponse:
+    pass
+
+def delete_car(car_id: int) -> JSONResponse:
+    pass
+
+routes = [
+    Route('/', method='GET', handler=list_cars)
+    Route('/', method='POST', handler=create_car)
+    Route('/{car_id}/', method='GET', handler=get_car)
+    Route('/{car_id}/', method='PUT', handler=update_car)
+    Route('/{car_id}/', method='DELETE', handler=delete_car)
+]
+
+app = App(routes=routes)
+
+if __name__ == '__main__':
+    app.serve('127.0.0.1', 5000, debug=True)
 
