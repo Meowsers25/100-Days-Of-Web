13c26a08679c7f27445a127ae1fe25548aad660c
Christian Lamothe <c.lamothe25@gmail.com>
2019-08-07 22:04:13 -0400

    Working on Pokemon API

--- 5-flask-api-calls/program/routes.py
+++ 5-flask-api-calls/program/routes.py
@@ -1,7 +1,8 @@
 from program import app
-from flask import render_template
+from flask import render_template, request
 from datetime import datetime
 import requests
+
 
 # time wont update if the variable is defined here
 # timenow = str(datetime.today())
@@ -24,7 +25,25 @@ def chuck():
   joke = get_chuck_joke()
   return render_template('chuck.html', joke=joke)
 
+@app.route('/pokemon', methods = ['GET', 'POST'])
+def pokemon():
+  pokemon = []
+  if request.method == 'POST' and 'pokecolor' in request.form:
+    color = request.form.get('pokecolor')
+    pokemon = get_poke_colors(color)
+  return render_template('pokemon.html', pokemon = pokemon)
+
 def get_chuck_joke():
   r = requests.get('https://api.chucknorris.io/jokes/random')
   data = r.json()
   return data['value']
+
+def get_poke_colors(color):
+  r = requests.get('https://pokeapi.co/api/v2/pokemon-color/' + color.lower())
+  pokedata = r.json()
+  pokemon = []
+
+  for i in pokedata['pokemon_species']:
+    pokemon.append(i['name'])
+
+  return pokemon
